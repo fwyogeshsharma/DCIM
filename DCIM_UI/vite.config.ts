d9391b13275110430f939f0da60d2e08c2f6829f
Aman yadav <yadavaman2282000@gmail.com>
2026-05-22 14:41:55 +0530

    vite config changes

--- DCIM_UI/vite.config.ts
+++ DCIM_UI/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  esbuild: {
+    minifyIdentifiers: false,
+  },
   server: {
     port: 5173,
     proxy: {
